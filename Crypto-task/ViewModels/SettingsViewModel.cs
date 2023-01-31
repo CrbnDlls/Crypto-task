70bd8c1cf737485339757f458d185cabd846154f
Pavlo Veremeienko <pavel.veremeienko@gmail.com>
2023-01-31 22:45:36 +0200

    Added russian language and language switch in settings

--- Crypto-task/ViewModels/SettingsViewModel.cs
+++ Crypto-task/ViewModels/SettingsViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Crypto_task.Helpers;
+using Crypto_task.Models;
 using Crypto_task.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.ApplicationModel;
+using Windows.Globalization;
 using Windows.UI;
 using Windows.UI.Xaml;
 
@@ -17,8 +21,27 @@ namespace Crypto_task.ViewModels
     public class SettingsViewModel : ObservableObject
     {
         private ElementTheme elementTheme = ThemeSelectorService.Theme;
-        
-        
+
+        private ObservableCollection<AppLanguage> languages;
+
+        private AppLanguage language;
+
+        public ObservableCollection<AppLanguage> Languages
+        {
+            get => languages;
+            set { languages = value; }
+        }
+
+        public AppLanguage SelectedLanguage
+        {
+            get => language;
+            set
+            {
+                SetProperty(ref language, value);
+                ApplicationLanguages.PrimaryLanguageOverride = value.Locale;
+            }
+        }
+
         public ElementTheme ElementTheme
         {
             get { return elementTheme; }
@@ -35,11 +58,18 @@ namespace Crypto_task.ViewModels
 
         public SettingsViewModel()
         {
+            languages = new ObservableCollection<AppLanguage> 
+            {
+                new AppLanguage { Name = "English", Locale = "en-US" },
+                new AppLanguage { Name = "Русский", Locale = "ru-RU" }
+            };
+
+            language = languages.FirstOrDefault(x => x.Locale.Contains(ApplicationLanguages.PrimaryLanguageOverride));
         }
 
-        public string[] Strings => GetStrings();
+        public string[] Themes => GetThemes();
 
-        public static string[] GetStrings()
+        public static string[] GetThemes()
         {
             List<string> list = new List<string>();
 
@@ -47,7 +77,6 @@ namespace Crypto_task.ViewModels
             {
                 list.Add(theme.ToString());
             }
-
 
             return list.ToArray();
         }
